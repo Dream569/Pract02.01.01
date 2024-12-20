3de3a21703c99a490f822ced9bd1c729d38c0c16
ИСП-41 <-41@LAB25-05>
2024-12-20 09:51:48 +0300

    Добавьте файлы проекта.

---
+++ Pract02.01/ClassPipi.cs
@@ -0,0 +1,28 @@
+﻿namespace Pract02._01
+{
+    public class ClassPipi
+    {
+        private static readonly Dictionary<char, int> CharacterValues = new Dictionary<char, int>
+    {
+        { 'А', -16 }, { 'Б', -15 }, { 'В', -14 }, { 'Г', -13 }, { 'Д', -12 },
+        { 'Е', -11 }, { 'Ё', -10 }, { 'Ж', -9 }, { 'З', -8 }, { 'И', -7 },
+        { 'Й', -6 }, { 'К', -5 }, { 'Л', -4 }, { 'М', -3 }, { 'Н', -2 },
+        { 'О', -1 }, { 'П', 0 }, { 'Р', 1 }, { 'С', 2 }, { 'Т', 3 },
+        { 'У', 4 }, { 'Ф', 5 }, { 'Х', 6 }, { 'Ц', 7 }, { 'Ч', 8 },
+        { 'Ш', 9 }, { 'Щ', 10 }, { 'Ь', 11 }, { 'Ю', 12 }, { 'Я', 13 }
+    };
+
+        public static List<int> GetNumericValues(string input)
+        {
+            List<int> numericValues = new List<int>();
+            foreach (char c in input)
+            {
+                if (CharacterValues.TryGetValue(c, out int value))
+                {
+                    numericValues.Add(value);
+                }
+            }
+            return numericValues;
+        }
+    }
+}
---
+++ Pract02.01/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using static Pract02._01.ClassPipi;
+
+namespace Pract02._01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void PlotButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = inputTextBox.Text;
+            List<int> values = ClassPipi.GetNumericValues(input);
+
+            if (values.Count == 0)
+            {
+                MessageBox.Show("Только русские ЗАГЛАВНЫЕ буквы!");
+                return;
+            }
+
+            DrawGraph(values);
+        }
+
+        private void DrawGraph(List<int> values)
+        {
+            graphCanvas.Children.Clear();
+            double canvasWidth = graphCanvas.ActualWidth;
+            double canvasHeight = graphCanvas.ActualHeight;
+
+            double max = double.MinValue;
+            double min = double.MaxValue;
+
+            foreach (int value in values)
+            {
+                if (value > max) max = value;
+                if (value < min) min = value;
+            }
+
+            double range = max - min;
+            if (range == 0) range = 1;
+
+            double xIncrement = canvasWidth / values.Count;
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                double x = i * xIncrement;
+                double y = canvasHeight - ((values[i] - min) / range * canvasHeight);
+
+                if (i > 0)
+                {
+                    Line line = new Line
+                    {
+                        X1 = (i - 1) * xIncrement,
+                        Y1 = canvasHeight - ((values[i - 1] - min) / range * canvasHeight),
+                        X2 = x,
+                        Y2 = y,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 2
+                    };
+                    graphCanvas.Children.Add(line);
+                }
+            }
+        }
+        private void InfoClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Это практическая работа заданная на практику: Пользователь вводит строку. Каждый символ преобразовать в \r\nцифровой показатель: А = -16, Б= -15 ... Я = 13 (через классы). Построить график \r\nна основании полученного «слова».");
+        }
+        private void ExitClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
